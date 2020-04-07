856e7bee7accdf2ba8484783c88f279c19155582
christoffertoppe <chris.mailbox85@gmail.com>
2020-04-07 22:51:08 +0300

    tehtävä 3 päivitetty

--- src/tehtava3/SortAlgorithms.java
+++ src/tehtava3/SortAlgorithms.java
@@ -12,56 +12,70 @@ package tehtava3;
 import java.util.Random;
 
 public class SortAlgorithms {
-    final static int MAX=1000;
+    static int MAX=15000;
+    static private int laskuri = 0;
 
     public static void main(String[] args) {
 
+    	
         selectSort();
         mergeSort();
+        quickSortLoop();
+    
+    	
     }
 
     public static void selectSort() {
 
         int[] taul = new int[MAX]; //taul tallettaa lajiteltavat tiedot
-        int i, j, k, apu, pienin;
+        int i, j, apu, pienin;
         Random r = new Random(); //luodaan satunnaislukugeneraattori
-        System.out.println("Generoidaan sy√∂tt√∂aineisto: ");
+    //    System.out.println("Generoidaan sy√∂tt√∂aineisto: ");
         for (i=0;i<MAX;i++) {
 
             taul[i] = r.nextInt(1000); //generoidaan luvut
-            System.out.print(taul[i]+" ");
-            if (i>0 && i%40==0) // rivinvaihto
-                System.out.println();
+   //         System.out.print(taul[i]+" ");
+   //         if (i>0 && i%40==0) // rivinvaihto
+   //             System.out.println();
         }
-        System.out.println("\nSuoritetaan valintalajittelu, paina Enter ");
-        Lue.merkki();
-        long startTime = System.currentTimeMillis();
+   //     System.out.println("\nSuoritetaan valintalajittelu, paina Enter ");
+   //     Lue.merkki();
+        laskuri++;
         for (i=0;i<MAX;i++) {
             pienin=i;
+            laskuri++;
             for (j=i+1;j<MAX;j++) {
+            	
                 /* l√∂ytyyk√∂ taulukon loppup√§√§st√§ pienemp√§√§ alkiota? */
+            	laskuri++;
                 if (taul[j] < taul[pienin]) {
                     pienin=j;
+               
                 }
+            laskuri++;
             }
+            
+            laskuri++;
             if (pienin != i) {
                 /* jos l√∂ytyi suoritetaan alkioiden vaihto */
                 apu=taul[pienin];
                 taul[pienin]=taul[i];
                 taul[i]=apu;
+                
             }
+            laskuri++; 
         }
-        System.out.println();
-        for (i=0;i<MAX;i++) {
-            System.out.print(taul[i]+" ");
-            if (i>0 && i%40==0) // rivinvaihto
-                System.out.println();
-        }
-        long endTime = System.currentTimeMillis();
-        long finalTime = endTime - startTime;
-        System.out.println("\nTime: " + finalTime);
-        System.out.println("\nKuittaa tulos, paina Enter ");
-        Lue.merkki();
+       
+  //      System.out.println();
+  //      for (i=0;i<MAX;i++) {
+  //          System.out.print(taul[i]+" ");
+  //          if (i>0 && i%40==0) // rivinvaihto
+  //              System.out.println();
+  //      }
+        System.out.println("Valintalajittelun vertailujen lukum‰‰r‰: " + laskuri);
+        laskuri = 0;
+    //    System.out.println("\nKuittaa tulos, paina Enter ");
+    //    Lue.merkki();
 
     }
 
@@ -69,25 +83,25 @@ public class SortAlgorithms {
         int[] a= new int[MAX];
         int i;
         Random r = new Random(); //luodaan satunnaislukugeneraattori
-        System.out.println("\nGeneroidaan sy√∂tt√∂aineisto: ");
+    //    System.out.println("\nGeneroidaan sy√∂tt√∂aineisto: ");
         for (i=0;i<MAX;i++) {
             a[i] = r.nextInt(1000); //generoidaan luvut
-            System.out.print(a[i]+" ");
-            if (i>0 && i%40==0) // rivinvaihto
-                System.out.println();
+       //     System.out.print(a[i]+" ");
+       //     if (i>0 && i%40==0) // rivinvaihto
+       //         System.out.println();
         }
-        System.out.println("\nSuoritetaan lomituslajittelu, paina Enter ");
-        Lue.merkki();
-        long startTime = System.currentTimeMillis();
+    //    System.out.println("\nSuoritetaan lomituslajittelu, paina Enter ");
+    //    Lue.merkki();
         mergeSort(a, 0, MAX-1);
-        for (i=0;i<MAX;i++) {
-            System.out.print(a[i]+" ");
-            if (i>0 && i%40==0) // rivinvaihto
-                System.out.println();
-        }
-    long endTime = System.currentTimeMillis();
-    long finalTime = endTime - startTime;
-    System.out.println("\nTime: " + finalTime); 
+    //    for (i=0;i<MAX;i++) {
+    //        System.out.print(a[i]+" ");
+    //        if (i>0 && i%40==0) // rivinvaihto
+    //            System.out.println();
+    //    }
+
+    System.out.println("Lomituslajittelun vertailujen lukum‰‰r‰: " + laskuri); 
+    laskuri = 0;
+   // System.out.println("\nKuittaa tulos, paina Enter ");
     }
 
 
@@ -98,30 +112,42 @@ public class SortAlgorithms {
         //i osoittaa 1. osataulukkoa, j osoittaa 2. osataulukkoa
         // k osoittaa aputaulukkoa, johon yhdiste kirjoitetaan.
         int i=p, j=q+1, k=0;
+        laskuri++;
         while(i<q+1 && j<r+1) {
+        		laskuri= laskuri+2;
                 if (t[i]<t[j]) {
                         tau[k++]=t[i++];
-                }
+                }           
                 else {
                         tau[k++]=t[j++];
                 }
+                laskuri++;
         }
         //toinen osataulukko k√§sitelty, siirret√§√§n toisen k√§sittelem√§tt√∂m√§t
+        laskuri++;
         while (i<q+1)
                 tau[k++]=t[i++];
+        		laskuri++;
+        laskuri++;		
         while (j<r+1)
                 tau[k++]=t[j++];
+        		laskuri++;
         //siirret√§√§n yhdiste alkuper√§iseen taulukkoon
+        laskuri++;		
         for (i=0;i<k;i++) {
                 t[p+i]=tau[i];
+                laskuri++;
         }
     }
+    
+    
 
     public static void mergeSort(int t[],  int alku,  int loppu) {
         int ositus;
         long la, ll, lt;
+        laskuri++;
         if (alku<loppu) { //onko v√§h. 2 alkiota, ett√§ voidaan suorittaa ositus
-
+        		
                 la=alku; ll=loppu;
                 lt=(la+ll)/2;
                 ositus=(int)lt;
@@ -130,6 +156,74 @@ public class SortAlgorithms {
                 merge(t, alku, ositus, loppu);//yhdistet√§√§n lajitellut osataulukot
         }
 
+    }
+    
+    public static void quickSortLoop() {
+        int taulukko[] = new int[MAX];
+        Random r = new Random();
+        for (int i = 0; i < MAX; i++) {
+            taulukko[i] = r.nextInt(1000);
+       //    System.out.print(taulukko[i] + " ");
+       //     if (i > 0 && i % 40 == 0) {// rivinvaihto
+       //         System.out.println();
+       //     }
+        }
+    //    System.out.println("\nJ‰rjestell‰‰n k‰ytt‰m‰ll‰ Quicksorttia, paina Enter");
+    //    Lue.merkki();
+        qs(taulukko, MAX);
+    //    System.out.println("Valmis");
+        System.out.println("Pikalajittelu vertailujen lukum‰‰r‰: " + laskuri + "\n");
+    }
+
+    public static void quickSort(int table[], int lo0, int hi0) {
+        int lo = lo0;
+        int hi = hi0;
+        int mid, swap;
+
+        mid = table[ (lo0 + hi0) / 2];
+        laskuri++;
+        while (lo <= hi) {
+        	laskuri++;
+            while (table[lo] < mid) {
+                ++lo;
+               laskuri++; 
+            }
+            laskuri++;
+            while (table[hi] > mid) {
+                --hi;
+                laskuri++;
+            }
+            laskuri++;
+            if (lo <= hi) {
+                swap = table[lo];
+                table[lo] = table[hi];
+                ++lo;
+                table[hi] = swap;
+                --hi;
+            }
+            laskuri++;
+        }
+        laskuri++;
+        if (lo0 < hi) {
+            quickSort(table, lo0, hi);
+        }
+        laskuri++;
+        if (lo < hi0) {
+            quickSort(table, lo, hi0);
+        }
+    }
+
+    public static void qs(int table[], int values) {
+        quickSort(table, 0, values - 1);
+
+    /*    System.out.println("\nJ‰rjestetty aineisto:\n");
+        for (int i = 0; i < values; i++) {
+            System.out.print(table[i] + " ");
+            if (i > 0 && i % 40 == 0) { // rivinvaihto
+                System.out.println();
+            }
+        } 
+    */
     }
     
     
